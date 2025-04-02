b6741b4220519ad0563624f90e126759f9278b0f
Uriel <uriel.alvarado@motivus.com>
2025-04-02 10:34:34 -0600

    repository using angular12
    repository using angular12

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { DoBootstrap, Injector, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { createCustomElement } from '@angular/elements';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  entryComponents: [AppComponent]
+  providers: [CommonModule,BrowserModule],
+  imports: [
+    BrowserModule // 🚀 Asegúrate de que está aquí
+  ],
 })
 export class AppModule implements DoBootstrap {
   constructor(injector: Injector) {
